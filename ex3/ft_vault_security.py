1253a4e0fd6bd8b72b59dcb515b7636eb23d83e8
Artem Sokolov <arsokolo@1-E-11.42heilbronn.de>
2026-06-03 14:41:48 +0200

    ex3: run all tests and finilized

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -1,16 +1,41 @@
 def secure_archive(
         filename: str,
         mode: str = "r",
-        content: str = None
-        ) -> tuple[True | False, str]:
-    pass
+        content: str | None = None
+        ) -> tuple[bool, str]:
+    try:
+        with open(filename, mode) as f:
+            if mode == "r":
+                return (True, str(f.read()))
+            elif mode == "w":
+                f.write(content)
+                return (True, "Content successfully written to file")
+            else:
+                return (False, f"Unknown mode '{mode}'")
+    except OSError as e:
+        return (False, str(e))
+    except TypeError as e:
+        return (False, f"No content provided for write: {e}")
 
 
 def main() -> None:
     print("=== Cyber Archives Security ===")
 
     print("Using 'secure_archive' to read from a nonexistent file:")
-    secure_archive()
+    print(secure_archive("not/existing"))
+
+    print("\nUsing 'secure_archive' to read from an inaccessible file:")
+    print(secure_archive("no_access"))
+
+    print("\nUsing 'secure_archive' with wrong mode:")
+    print(secure_archive("ancient_fragment.txt", "a"))
+
+    print("\nUsing 'secure_archive' to read from a regular file:")
+    output = secure_archive("ancient_fragment.txt")
+    print(output)
+
+    print("\nUsing 'secure_archive' to write previous content to a new file:")
+    print(secure_archive("new_file.txt", "w", output[1]))
 
 
 if __name__ == "__main__":
